597064fba01f65b15f7ac2aa81cadaa44972f4a0
lidanqing-intel <danqing.li@intel.com>
2020-11-11 08:55:49 +0100

    update

---
+++ life_experience/exercise_suit_me.cc
@@ -0,0 +1,1 @@
+When taking fast walk, don't walk, but jump each leg like a kid. It is very interesting and suit me. At the same time, sing different songs.
---
+++ virtual_function/virtual_function.cc
@@ -0,0 +1,13 @@
+//[Definition]
+// virtual function is declared within a base class 
+// and is redefined(Override) by a derived class.
+// You can refer a derived object with a base class pointer or reference to call a virtual function
+// to execute the derived class's version of function
+//
+//[Note]
+// virtual functions are mainly used to achieve Runtime Polymorphism
+// The resolving of function call is done at Run-time.
+//
+//[Rule]
+//
+//
